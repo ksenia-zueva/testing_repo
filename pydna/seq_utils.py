5fd0e850624fa009420d42cf81f050bc8b551ca3
Ksenia Zueva <ksezue@utu.fi>
2015-04-23 10:32:04 +0400

    creating python module

--- pydna/seq_utils.py
+++ pydna/seq_utils.py
@@ -1,15 +1,23 @@
 def complement_codon(input_codon):
+    answer = is_codon_correct(input_codon)
+    if answer is True:
+       print('Things working nicely')
+    else:
+        print('Codon has bad characters')
+        return None
+        
     base_complements = {
             'A': 'T',
             'T': 'A',
             'C': 'G',
             'G': 'C',
+            '?': '?',
     }
 
     first_base = input_codon[0]
     second_base = input_codon[1]
     third_base = input_codon[2]
-
+        
     first_complemented_base = base_complements[first_base]
     second_complemented_base = base_complements[second_base]
     third_complemented_base = base_complements[third_base]
@@ -18,3 +26,14 @@ def complement_codon(input_codon):
 
     return complemented_codon
 
+def is_codon_correct(input_codon):    
+    allowed_characters = ['A','T','C','G','?','N','_']
+    
+    for base in input_codon:
+       if base.upper() in allowed_characters:
+           continue
+       else:
+          print('Your codon is bad')
+          return False
+    return True
+ 
